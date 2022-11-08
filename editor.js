8594f8918c234fa9a18ad7d0473f15920cbd2392
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-11-08 14:23:51 -0800

    Configurações por .editorconfig, adição de delimitador nulo ao final da linha para evitar problemas com o Lexador.

--- editor.js
+++ editor.js
@@ -3,18 +3,18 @@ const runButton = document.getElementById("runBtn");
 const demoSelector = document.getElementById("demoSelector");
 
 String.prototype.capitalize = function () {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+    return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
 function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split("=");
-    if (decodeURIComponent(pair[0]) == variable) {
-      return decodeURIComponent(pair[1]);
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
     }
-  }
 }
 
 // console.log = console.error = function (msg) {
@@ -24,67 +24,72 @@ function getQueryVariable(variable) {
 //   outputDiv.appendChild(p);
 // };
 
-const mostrarResultado = function (msg){
-  const p = document.createElement("p");
-  p.textContent = msg;
-  p.classList = " output";
-  outputDiv.appendChild(p);
-}
+const mostrarResultado = function (msg) {
+    const p = document.createElement("p");
+    p.textContent = msg;
+    p.classList = " output";
+    outputDiv.appendChild(p);
+};
 
 const clearOutput = function () {
-  outputDiv.innerHTML = "";
+    outputDiv.innerHTML = "";
 };
 
 const editor = new CodeFlask("#editor", {
-  language: "js",
-  lineNumbers: true,
-  defaultTheme: false,
+    language: "js",
+    lineNumbers: true,
+    defaultTheme: false,
 });
 
 clearOutput();
 
 const demoKeys = Object.keys(demos);
 function loadDemo(name) {
-  editor.updateCode(demos[name]);
+    editor.updateCode(demos[name]);
 }
 
 demoKeys.forEach((demo, index) => {
-  const option = document.createElement("option");
-  if (index === 0) {
-    option.disabled = true;
-    option.selected = true;
-    option.hidden = true;
-  }
+    const option = document.createElement("option");
+    if (index === 0) {
+        option.disabled = true;
+        option.selected = true;
+        option.hidden = true;
+    }
 
-  option.textContent = demo.capitalize();
-  option.value = demo;
-  demoSelector.appendChild(option);
+    option.textContent = demo.capitalize();
+    option.value = demo;
+    demoSelector.appendChild(option);
 });
 
 let queryCode = getQueryVariable("code");
 if (queryCode !== undefined) {
-  editor.updateCode(decodeURI(queryCode));
-  demoSelector.value = "custom";
+    editor.updateCode(decodeURI(queryCode));
+    demoSelector.value = "custom";
 } else {
-  loadDemo(demoKeys[0]);
+    loadDemo(demoKeys[0]);
 }
 
 const executarCodigo = function () {
-  const delegua = new Delegua.Delegua('', mostrarResultado);
+    const delegua = new Delegua.DeleguaWeb("", mostrarResultado);
 
-  const codigo = editor.getCode().split("\n");
+    const codigo = editor.getCode().split("\n");
 
-  const retornoLexador = delegua.lexador.mapear(codigo, -1);
-  const retornoAvaliadorSintatico = delegua.avaliadorSintatico.analisar(retornoLexador);
+    for (let linha = 0; linha < codigo.length; linha++) {
+        codigo[linha] += "\0";
+    }
 
-  delegua.executar({ retornoLexador, retornoAvaliadorSintatico });
+    const retornoLexador = delegua.lexador.mapear(codigo, -1);
+    const retornoAvaliadorSintatico =
+        delegua.avaliadorSintatico.analisar(retornoLexador);
+
+    delegua.executar({ retornoLexador, retornoAvaliadorSintatico });
 };
 
 demoSelector.addEventListener("change", function () {
-  loadDemo(demoSelector.value);
+    loadDemo(demoSelector.value);
 });
 
 runButton.addEventListener("click", function () {
-  clearOutput();
-  executarCodigo();
+    clearOutput();
+    executarCodigo();
 });
--- index.ts
+++ index.ts
@@ -2,63 +2,82 @@ import { Lexador } from "@designliquido/delegua/fontes/lexador";
 import { AvaliadorSintatico } from "@designliquido/delegua/fontes/avaliador-sintatico";
 import { Interpretador } from "@designliquido/delegua/fontes/interpretador";
 import tiposDeSimbolos from "@designliquido/delegua/fontes/tipos-de-simbolos/delegua";
-import { 
-  AvaliadorSintaticoInterface,
-  DeleguaInterface,
-  ImportadorInterface,
-  InterpretadorInterface,
-  LexadorInterface,
-  RetornoExecucaoInterface,
-  SimboloInterface
+import {
+    AvaliadorSintaticoInterface,
+    DeleguaInterface,
+    ImportadorInterface,
+    InterpretadorInterface,
+    LexadorInterface,
+    RetornoExecucaoInterface,
+    SimboloInterface,
 } from "@designliquido/delegua/fontes/interfaces/index";
-import { RetornoImportador } from '@designliquido/delegua/fontes/importador';
+import { RetornoImportador } from "@designliquido/delegua/fontes/importador";
 
-export class Delegua implements DeleguaInterface {
-  nomeArquivo: string;
+export class DeleguaWeb implements DeleguaInterface {
+    nomeArquivo: string;
 
-  teveErro: boolean;
-  teveErroEmTempoDeExecucao: boolean;
-  // TODO: Remover todos os `any` abaixo depois de implementar DeleguaInterface.
-  dialeto: string = 'delegua';
-  arquivosAbertos: any;
-  interpretador: InterpretadorInterface;
-  lexador: LexadorInterface;
-  avaliadorSintatico: AvaliadorSintaticoInterface;
-  importador: ImportadorInterface;
-  funcaoDeRetorno: Function;
-  iniciarDelegua: any;
-  carregarArquivo: any;
-  conteudoArquivosAbertos: any;
-  executarUmaLinha: any;
+    teveErro: boolean;
+    teveErroEmTempoDeExecucao: boolean;
+    // TODO: Remover todos os `any` abaixo depois de implementar DeleguaInterface.
+    dialeto: string = "delegua";
+    arquivosAbertos: any;
+    interpretador: InterpretadorInterface;
+    lexador: LexadorInterface;
+    avaliadorSintatico: AvaliadorSintaticoInterface;
+    importador: ImportadorInterface;
+    funcaoDeRetorno: Function;
+    iniciarDelegua: any;
+    carregarArquivo: any;
+    conteudoArquivosAbertos: any;
+    executarUmaLinha: any;
 
-  constructor(nomeArquivo: string, funcaoDeRetorno: Function = null) {
-    this.nomeArquivo = nomeArquivo;
-    this.funcaoDeRetorno = funcaoDeRetorno || console.log;
+    constructor(nomeArquivo: string, funcaoDeRetorno: Function = null) {
+        this.nomeArquivo = nomeArquivo;
+        this.funcaoDeRetorno = funcaoDeRetorno || console.log;
 
-    this.lexador = new Lexador();
-    this.avaliadorSintatico = new AvaliadorSintatico();
-    this.interpretador = new Interpretador(null, '', false, this.funcaoDeRetorno);
+        this.lexador = new Lexador();
+        this.avaliadorSintatico = new AvaliadorSintatico();
+        this.interpretador = new Interpretador(
+            null,
+            "",
+            false,
+            this.funcaoDeRetorno
+        );
 
-    this.teveErro = false;
-    this.teveErroEmTempoDeExecucao = false;
-  }
+        this.teveErro = false;
+        this.teveErroEmTempoDeExecucao = false;
+    }
 
-    executar(retornoImportador: RetornoImportador, manterAmbiente: boolean = false): RetornoExecucaoInterface {
+    executar(
+        retornoImportador: RetornoImportador,
+        manterAmbiente: boolean = false
+    ): RetornoExecucaoInterface {
         if (retornoImportador.retornoLexador.erros.length > 0) {
             for (const erroLexador of retornoImportador.retornoLexador.erros) {
-                this.reportar(erroLexador.linha, ` no '${erroLexador.caractere}'`, erroLexador.mensagem);
+                this.reportar(
+                    erroLexador.linha,
+                    ` no '${erroLexador.caractere}'`,
+                    erroLexador.mensagem
+                );
             }
             return;
         }
 
         if (retornoImportador.retornoAvaliadorSintatico.erros.length > 0) {
-            for (const erroAvaliadorSintatico of retornoImportador.retornoAvaliadorSintatico.erros) {
-                this.erro(erroAvaliadorSintatico.simbolo, erroAvaliadorSintatico.message);
+            for (const erroAvaliadorSintatico of retornoImportador
+                .retornoAvaliadorSintatico.erros) {
+                this.erro(
+                    erroAvaliadorSintatico.simbolo,
+                    erroAvaliadorSintatico.message
+                );
             }
             return;
         }
 
-        const retornoInterpretador = this.interpretador.interpretar(retornoImportador.retornoAvaliadorSintatico.declaracoes, manterAmbiente);
+        const retornoInterpretador = this.interpretador.interpretar(
+            retornoImportador.retornoAvaliadorSintatico.declaracoes,
+            manterAmbiente
+        );
 
         if (retornoInterpretador.erros.length > 0) {
             for (const erroInterpretador of retornoInterpretador.erros) {
@@ -66,8 +85,12 @@ export class Delegua implements DeleguaInterface {
                     this.erroEmTempoDeExecucao(erroInterpretador.simbolo);
                 } else {
                     const erroEmJavaScript: any = erroInterpretador as any;
-                    console.error(`Erro em JavaScript: ` + `${erroEmJavaScript.message}`);
-                    console.error(`Pilha de execução: ` + `${erroEmJavaScript.stack}`);
+                    console.error(
+                        `Erro em JavaScript: ` + `${erroEmJavaScript.message}`
+                    );
+                    console.error(
+                        `Pilha de execução: ` + `${erroEmJavaScript.stack}`
+                    );
                 }
             }
         }
@@ -78,45 +101,49 @@ export class Delegua implements DeleguaInterface {
         };
     }
 
-    versao(){
-        return '0.8'
+    versao() {
+        return "0.8";
     }
 
-  reportar(linha: number, onde: any, mensagem: string) {
-    if (this.nomeArquivo)
-        console.error(
-            `[Arquivo: ${this.nomeArquivo}] [Linha: ${linha}]` + ` Erro${onde}: ${mensagem}`
-        );
-    else console.error(`[Linha: ${linha}]` +  ` Erro${onde}: ${mensagem}`);
-    this.teveErro = true;
-  }
-
-  erro(simbolo: SimboloInterface, mensagemDeErro: string) {
-    if (simbolo.tipo === tiposDeSimbolos.EOF) {
-        this.reportar(Number(simbolo.linha), ' no final', mensagemDeErro);
-    } else {
-        this.reportar(
-            Number(simbolo.linha),
-            ` no '${simbolo.lexema}'`,
-            mensagemDeErro
-        );
-    }
-  }
-
-  erroEmTempoDeExecucao(erro: any): void {
-    if (erro && erro.simbolo && erro.simbolo.linha) {
+    reportar(linha: number, onde: any, mensagem: string) {
         if (this.nomeArquivo)
             console.error(
-                `Erro: [Arquivo: ${this.nomeArquivo}] [Linha: ${erro.simbolo.linha}]` + ` ${erro.mensagem}`
+                `[Arquivo: ${this.nomeArquivo}] [Linha: ${linha}]` +
+                    ` Erro${onde}: ${mensagem}`
             );
-        else
-            console.error(
-                `Erro: [Linha: ${erro.simbolo.linha}]` + ` ${erro.mensagem}`
-            );
-    } else {
-        console.error(`Erro: [Linha: ${erro.linha || 0}]` + ` ${erro.mensagem}`);
+        else console.error(`[Linha: ${linha}]` + ` Erro${onde}: ${mensagem}`);
+        this.teveErro = true;
     }
 
-    this.teveErroEmTempoDeExecucao = true;
-  }
+    erro(simbolo: SimboloInterface, mensagemDeErro: string) {
+        if (simbolo.tipo === tiposDeSimbolos.EOF) {
+            this.reportar(Number(simbolo.linha), " no final", mensagemDeErro);
+        } else {
+            this.reportar(
+                Number(simbolo.linha),
+                ` no '${simbolo.lexema}'`,
+                mensagemDeErro
+            );
+        }
+    }
+
+    erroEmTempoDeExecucao(erro: any): void {
+        if (erro && erro.simbolo && erro.simbolo.linha) {
+            if (this.nomeArquivo)
+                console.error(
+                    `Erro: [Arquivo: ${this.nomeArquivo}] [Linha: ${erro.simbolo.linha}]` +
+                        ` ${erro.mensagem}`
+                );
+            else
+                console.error(
+                    `Erro: [Linha: ${erro.simbolo.linha}]` + ` ${erro.mensagem}`
+                );
+        } else {
+            console.error(
+                `Erro: [Linha: ${erro.linha || 0}]` + ` ${erro.mensagem}`
+            );
+        }
+
+        this.teveErroEmTempoDeExecucao = true;
+    }
 }
