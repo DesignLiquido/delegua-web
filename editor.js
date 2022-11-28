b3f1000eefd725a138b1f98d731948c80af6f215
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2022-11-28 10:11:00 -0800

    Updates

--- editor.js
+++ editor.js
@@ -32,38 +32,6 @@ var editor = new CodeFlask("#editor", {
     defaultTheme: false,
 });
 clearOutput();
-var objetoDemonstracoes = Object.entries(demos);
-function carregarDemonstracao(grupo, nome) {
-    editor.updateCode(demos[grupo][nome]);
-}
-var opcaoPadrao = document.createElement("option");
-opcaoPadrao.disabled = true;
-opcaoPadrao.selected = true;
-opcaoPadrao.hidden = true;
-opcaoPadrao.textContent = capitalize('Exemplos...');
-opcaoPadrao.value = 'Exemplos...';
-seletorDemos.appendChild(opcaoPadrao);
-objetoDemonstracoes.forEach(function (_a) {
-    var chave = _a[0], valor = _a[1];
-    var optgroup = document.createElement("optgroup");
-    optgroup.textContent = capitalize(chave);
-    optgroup.label = chave;
-    Object.entries(valor).forEach(function (_a) {
-        var exemplo = _a[0], codigo = _a[1];
-        var opcao = document.createElement("option");
-        opcao.textContent = exemplo;
-        optgroup.appendChild(opcao);
-    });
-    seletorDemos.appendChild(optgroup);
-});
-var queryCode = getQueryVariable("code");
-if (queryCode !== undefined) {
-    editor.updateCode(decodeURI(queryCode));
-    seletorDemos.value = "custom";
-}
-else {
-    carregarDemonstracao('Fundamentos', 'Olá Mundo');
-}
 var executarCodigo = function () {
     var delegua = new Delegua.DeleguaWeb("", mostrarResultado);
     var codigo = editor.getCode().split("\n");
@@ -74,10 +42,6 @@ var executarCodigo = function () {
     var retornoAvaliadorSintatico = delegua.avaliadorSintatico.analisar(retornoLexador);
     delegua.executar({ retornoLexador: retornoLexador, retornoAvaliadorSintatico: retornoAvaliadorSintatico });
 };
-seletorDemos.addEventListener("change", function (e) {
-    var grupoSelecionado = document.querySelector('#seletorDemos option:checked').parentElement.label;
-    carregarDemonstracao(grupoSelecionado, e.target.value);
-});
 runButton.addEventListener("click", function () {
     clearOutput();
     executarCodigo();
