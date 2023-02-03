e38f5eec441068a140333eb6db357bbc491bc1e9
Samuel Vaz <samuel.rvg@gmail.com>
2023-02-03 16:44:16 -0300

    gramatica base de delégua

--- exemplos.ts
+++ exemplos.ts
@@ -250,88 +250,361 @@ enfileirar(valorEntrada);
 mostrar_fila();`,
 }
 
+    function definicaoLinguagemDelegua() {
+        return {
+          defaultToken: 'invalid',
+          tokenPostfix: '.ts',
+        
+          keywords: [
+            // Should match the keys of textToKeywordObj in
+            // https://github.com/microsoft/TypeScript/blob/master/src/compiler/scanner.ts
+            'abstract',
+            'any',
+            'as',
+            'asserts',
+            'bigint',
+            'boolean',
+            'break',
+            'case',
+            'catch',
+            'class',
+            'continue',
+            'const',
+            'constructor',
+            'debugger',
+            'declare',
+            'default',
+            'delete',
+            'do',
+            'else',
+            'enum',
+            'export',
+            'extends',
+            'false',
+            'finally',
+            'for',
+            'from',
+            'function',
+            'get',
+            'if',
+            'implements',
+            'import',
+            'in',
+            'infer',
+            'instanceof',
+            'interface',
+            'is',
+            'keyof',
+            'let',
+            'module',
+            'namespace',
+            'never',
+            'new',
+            'null',
+            'number',
+            'object',
+            'out',
+            'package',
+            'private',
+            'protected',
+            'public',
+            'override',
+            'readonly',
+            'require',
+            'global',
+            'return',
+            'satisfies',
+            'set',
+            'static',
+            'string',
+            'super',
+            'switch',
+            'symbol',
+            'this',
+            'throw',
+            'true',
+            'try',
+            'type',
+            'typeof',
+            'undefined',
+            'unique',
+            'unknown',
+            'var',
+            'void',
+            'while',
+            'with',
+            'yield',
+            'async',
+            'await',
+            'of'
+          ],
+        
+          operators: [
+            '<=',
+            '>=',
+            '==',
+            '!=',
+            '===',
+            '!==',
+            '=>',
+            '+',
+            '-',
+            '**',
+            '*',
+            '/',
+            '%',
+            '++',
+            '--',
+            '<<',
+            '</',
+            '>>',
+            '>>>',
+            '&',
+            '|',
+            '^',
+            '!',
+            '~',
+            '&&',
+            '||',
+            '??',
+            '?',
+            ':',
+            '=',
+            '+=',
+            '-=',
+            '*=',
+            '**=',
+            '/=',
+            '%=',
+            '<<=',
+            '>>=',
+            '>>>=',
+            '&=',
+            '|=',
+            '^=',
+            '@'
+          ],
+        
+          // we include these common regular expressions
+          symbols: /[=><!~?:&|+\-*\/\^%]+/,
+          escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+          digits: /\d+(_+\d+)*/,
+          octaldigits: /[0-7]+(_+[0-7]+)*/,
+          binarydigits: /[0-1]+(_+[0-1]+)*/,
+          hexdigits: /[[0-9a-fA-F]+(_+[0-9a-fA-F]+)*/,
+        
+          regexpctl: /[(){}\[\]\$\^|\-*+?\.]/,
+          regexpesc: /\\(?:[bBdDfnrstvwWn0\\\/]|@regexpctl|c[A-Z]|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4})/,
+
+            // The main tokenizer for our languages
+            tokenizer: {
+              root: [[/[{}]/, 'delimiter.bracket'], { include: 'common' }],
+
+              common: [
+                // identifiers and keywords
+                [
+                  /[a-z_$][\w$]*/,
+                  {
+                    cases: {
+                      '@keywords': 'keyword',
+                      '@default': 'identifier'
+                    }
+                  }
+                ],
+                [/[A-Z][\w\$]*/, 'type.identifier'], // to show class names nicely
+                // [/[A-Z][\w\$]*/, 'identifier'],
+          
+                // whitespace
+                { include: '@whitespace' },
+          
+                // regular expression: ensure it is terminated before beginning (otherwise it is an opeator)
+                [
+                  /\/(?=([^\\\/]|\\.)+\/([dgimsuy]*)(\s*)(\.|;|,|\)|\]|\}|$))/,
+                  { token: 'regexp', bracket: '@open', next: '@regexp' }
+                ],
+          
+                // delimiters and operators
+                [/[()\[\]]/, '@brackets'],
+                [/[<>](?!@symbols)/, '@brackets'],
+                [/!(?=([^=]|$))/, 'delimiter'],
+                [
+                  /@symbols/,
+                  {
+                    cases: {
+                      '@operators': 'delimiter',
+                      '@default': ''
+                    }
+                  }
+                ],
+          
+                // numbers
+                [/(@digits)[eE]([\-+]?(@digits))?/, 'number.float'],
+                [/(@digits)\.(@digits)([eE][\-+]?(@digits))?/, 'number.float'],
+                [/0[xX](@hexdigits)n?/, 'number.hex'],
+                [/0[oO]?(@octaldigits)n?/, 'number.octal'],
+                [/0[bB](@binarydigits)n?/, 'number.binary'],
+                [/(@digits)n?/, 'number'],
+          
+                // delimiter: after number because of .\d floats
+                [/[;,.]/, 'delimiter'],
+          
+                // strings
+                [/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+                [/'([^'\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+                [/"/, 'string', '@string_double'],
+                [/'/, 'string', '@string_single'],
+                [/`/, 'string', '@string_backtick']
+              ],
+          
+              whitespace: [
+                [/[ \t\r\n]+/, ''],
+                [/\/\*\*(?!\/)/, 'comment.doc', '@jsdoc'],
+                [/\/\*/, 'comment', '@comment'],
+                [/\/\/.*$/, 'comment']
+              ],
+          
+              comment: [
+                [/[^\/*]+/, 'comment'],
+                [/\*\//, 'comment', '@pop'],
+                [/[\/*]/, 'comment']
+              ],
+          
+              jsdoc: [
+                [/[^\/*]+/, 'comment.doc'],
+                [/\*\//, 'comment.doc', '@pop'],
+                [/[\/*]/, 'comment.doc']
+              ],
+          
+              // We match regular expression quite precisely
+              regexp: [
+                [
+                  /(\{)(\d+(?:,\d*)?)(\})/,
+                  ['regexp.escape.control', 'regexp.escape.control', 'regexp.escape.control']
+                ],
+                [
+                  /(\[)(\^?)(?=(?:[^\]\\\/]|\\.)+)/,
+                  ['regexp.escape.control', { token: 'regexp.escape.control', next: '@regexrange' }]
+                ],
+                [/(\()(\?:|\?=|\?!)/, ['regexp.escape.control', 'regexp.escape.control']],
+                [/[()]/, 'regexp.escape.control'],
+                [/@regexpctl/, 'regexp.escape.control'],
+                [/[^\\\/]/, 'regexp'],
+                [/@regexpesc/, 'regexp.escape'],
+                [/\\\./, 'regexp.invalid'],
+                [/(\/)([dgimsuy]*)/, [{ token: 'regexp', bracket: '@close', next: '@pop' }, 'keyword.other']]
+              ],
+          
+              regexrange: [
+                [/-/, 'regexp.escape.control'],
+                [/\^/, 'regexp.invalid'],
+                [/@regexpesc/, 'regexp.escape'],
+                [/[^\]]/, 'regexp'],
+                [
+                  /\]/,
+                  {
+                    token: 'regexp.escape.control',
+                    next: '@pop',
+                    bracket: '@close'
+                  }
+                ]
+              ],
+          
+              string_double: [
+                [/[^\\"]+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/"/, 'string', '@pop']
+              ],
+          
+              string_single: [
+                [/[^\\']+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/'/, 'string', '@pop']
+              ],
+          
+              string_backtick: [
+                [/\$\{/, { token: 'delimiter.bracket', next: '@bracketCounting' }],
+                [/[^\\`$]+/, 'string'],
+                [/@escapes/, 'string.escape'],
+                [/\\./, 'string.escape.invalid'],
+                [/`/, 'string', '@pop']
+              ],
+          
+              bracketCounting: [
+                [/\{/, 'delimiter.bracket', '@bracketCounting'],
+                [/\}/, 'delimiter.bracket', '@pop'],
+                { include: 'common' }
+              ]
+            }
+        };
+    }
+
 window.onload = function () {
+
+  const arr = [
+    '//teste',
+    '/*outro teste*/',
+    'var a = \'Oi\'',
+    'var b = 12',
+    'escreva(\'Oii\')',
+  ].join('\n');
+
   const exemploId: any = window.location.search.split('?exemploId=')[1];
 
   const Monaco = (window as any).monaco;
 
-  //TODO: @Samuel
   Monaco.languages.register({
-    id: 'delegua',
-    extensions: ['.delegua'],
-    aliases: ['delegua'],
-    mimetypes: ['application/delegua'],
+    id: 'delegua'
   });
-  Monaco.languages.setLanguageConfiguration('delegua', {
-    wordPattern: /(-?\d*\.\d\w*)|([^\[\{\]\}\:\"\,\s]+)/g,
+  Monaco.languages.setMonarchTokensProvider('delegua', definicaoLinguagemDelegua());
 
-    comments: {
-      lineComment: '//',
-      blockComment: ['/*', '*/']
-    },
+  Monaco.editor.create(document.getElementById('editor'), {
+    value: arr,
+    language: 'delegua'
+  });
   
-    brackets: [
-      ['{', '}'],
-      ['[', ']']
-    ],
-  
-    autoClosingPairs: [
-      { open: '{', close: '}', notIn: ['string'] },
-      { open: '[', close: ']', notIn: ['string'] },
-      { open: '"', close: '"', notIn: ['string'] }
-    ]
-  });
+  // Monaco.languages.register({
+  //   id: 'delegua',
+  //   extensions: ['.delegua'],
+  //   aliases: ['delegua', 'language-generation'],
+  //   mimetypes: ['application/delegua'],
+  // });
 
-  Monaco.languages.setMonarchTokensProvider('delegua', {
-    tokenizer: {
-      root: [
-        [/\[error.*/, 'custom-error'],
-        [/\[notice.*/, 'custom-notice'],
-        [/\[info.*/, 'custom-info'],
-        [/\[[a-zA-Z 0-9:]+\]/, 'custom-date']
-      ]
-    }
-  });
+  // Monaco.languages.registerCompletionItemProvider('delegua', {
+  //   provideCompletionItems: () => {
+  //     var suggestions = [{
+  //       label: 'simpleText',
+  //       kind: Monaco.languages.CompletionItemKind.Text,
+  //       insertText: 'simpleText'
+  //     }, {
+  //       label: 'testing',
+  //       kind: Monaco.languages.CompletionItemKind.Keyword,
+  //       insertText: 'testing(${1:condition})',
+  //       insertTextRules: Monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  //     }, {
+  //       label: '',
+  //       kind: Monaco.languages.CompletionItemKind.Snippet,
+  //       insertText: [
+  //         'if (${1:condition}) {',
+  //         '\t$0',
+  //         '} else {',
+  //         '\t',
+  //         '}'
+  //       ].join('\n'),
+  //       insertTextRules: Monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+  //       documentation: 'If-Else Statement'
+  //     }];
+  //     return { suggestions: suggestions };
+  //   }
+  // });
 
-  Monaco.languages.registerCompletionItemProvider('delegua', {
-    provideCompletionItems: () => {
-      var suggestions = [{
-        label: 'simpleText',
-        kind: Monaco.languages.CompletionItemKind.Text,
-        insertText: 'simpleText'
-      }, {
-        label: 'testing',
-        kind: Monaco.languages.CompletionItemKind.Keyword,
-        insertText: 'testing(${1:condition})',
-        insertTextRules: Monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
-      }, {
-        label: '',
-        kind: Monaco.languages.CompletionItemKind.Snippet,
-        insertText: [
-          'if (${1:condition}) {',
-          '\t$0',
-          '} else {',
-          '\t',
-          '}'
-        ].join('\n'),
-        insertTextRules: Monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-        documentation: 'If-Else Statement'
-      }];
-      return { suggestions: suggestions };
-    }
-  });
-
-  Monaco.languages.registerHoverProvider('delegua', {
-    provideHover: function(model, position) { 
-      // Log the current word in the console, you probably want to do something else here.
-      console.log(model.getWordAtPosition(position));
-    }
-  })
-
-  Monaco.editor.create(document.getElementById("editor"), {
-      value: Exemplos[exemploId],
-      language: "delegua",
-  });
+  // Monaco.languages.registerHoverProvider('delegua', {
+  //   provideHover: function(model, position) { 
+  //     // Log the current word in the console, you probably want to do something else here.
+  //     console.log(model.getWordAtPosition(position));
+  //   }
+  // })
 
   if(exemploId){
     document.querySelector('#titulo-arquivo').innerHTML = `${exemploId}.delegua`;
