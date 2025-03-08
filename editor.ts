4eba8ed02b1cc0060213d1592c8f1c8f942f17bd
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-03-08 10:52:12 -0800

    Separação entre avisos e erros. Avaliação sintática sempre devolve erros. Análise semântica sempre devolve avisos.

--- editor.ts
+++ editor.ts
@@ -34,16 +34,37 @@ const mapearErros = function (erros: any[]) {
 
     const _erros = erros.map(item => {
         return {
-            startLineNumber: item?.simbolo?.linha || item.linha,
+            startLineNumber: item.simbolo?.linha || item.linha,
             startColumn: 1,
             endLineNumber: 2,
             endColumn: 1000,
-            message: item?.mensagem || item.erroInterno,
+            message: item.message || item.mensagem || item.erroInterno,
             severity: MarkerSeverity.Error
         }
     })
 
     Monaco.editor.setModelMarkers(editor.getModel(), 'delegua', _erros);
+}
+
+const mapearAvisos = function (avisos: any[]) {
+    const editor = Monaco?.editor.getEditors()[0];
+
+    if (avisos.length > 0) {
+        console.log(avisos);
+    }
+
+    const _avisos = avisos.map(item => {
+        return {
+            startLineNumber: item.simbolo?.linha || item.linha,
+            startColumn: 1,
+            endLineNumber: 2,
+            endColumn: 1000,
+            message: item.message || item.mensagem || item.erroInterno,
+            severity: MarkerSeverity.Warning
+        }
+    })
+
+    Monaco.editor.setModelMarkers(editor.getModel(), 'delegua', _avisos);
 }
 
 const executarTradutor = function () {
@@ -85,15 +106,19 @@ const executarCodigo = async function () {
         const retornoLexador = delegua.lexador.mapear(codigo, -1);
         const retornoAvaliadorSintatico =
             delegua.avaliadorSintatico.analisar(retornoLexador);
+        if (retornoAvaliadorSintatico.erros.length > 0) {
+            return mapearErros(retornoAvaliadorSintatico.erros);
+        }
+
         const analisadorSemantico = delegua.analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes);
         const errosAnaliseSemantica = analisadorSemantico.diagnosticos;
 
         if (errosAnaliseSemantica?.length) {
-            return mapearErros(errosAnaliseSemantica);
-        } else {
-            const editor = Monaco?.editor.getEditors()[0];
-            Monaco.editor.setModelMarkers(editor.getModel(), 'delegua', []);
+            return mapearAvisos(errosAnaliseSemantica);
         }
+
+        const editor = Monaco?.editor.getEditors()[0];
+        Monaco.editor.setModelMarkers(editor.getModel(), 'delegua', []);
 
         const respostaInterpretador = await delegua.executar({ retornoLexador, retornoAvaliadorSintatico });
         const errosInterpretacao = respostaInterpretador.erros;
@@ -118,6 +143,11 @@ const analisarCodigo = function () {
 
     const retornoLexador = delegua.lexador.mapear(codigo, -1);
     const retornoAvaliadorSintatico = delegua.avaliadorSintatico.analisar(retornoLexador);
+    if (retornoAvaliadorSintatico.erros.length > 0) {
+        mapearErros(retornoAvaliadorSintatico.erros);
+        return;
+    }
+
     const analisadorSemantico = delegua.analisadorSemantico.analisar(retornoAvaliadorSintatico.declaracoes);
     const errosAnaliseSemantica = analisadorSemantico.diagnosticos;
 
