b2547a4ede0ef3df9b21689ee330b39f3c12226c
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2025-05-26 10:52:58 -0700

    Atualização do núcleo de Delégua para a versão 0.41.5.

--- index.ts
+++ index.ts
@@ -147,7 +147,7 @@ export class DeleguaWeb {
     }
 
     versao() {
-        return "0.39 (web)";
+        return "0.41 (web)";
     }
 
     reportar(linha: number, onde: any, mensagem: string) {
