b7e02229d733aec7023ae107a66a91e8a77a0ff6
Leonel Sanches da Silva <leonel@designliquido.com.br>
2025-12-13 09:57:42 -0800

    Tradução dos mecanismos do menu.

--- menu.ts
+++ menu.ts
@@ -1,7 +1,7 @@
-function openNav() {
-  document.getElementById("mySidenav")!.style.width = "100%";
+function abrirMenu() {
+  document.getElementById("menuOpcoes")!.style.width = "100%";
 }
 
-function closeNav() {
-  document.getElementById("mySidenav")!.style.width = "0";
+function fecharMenu() {
+  document.getElementById("menuOpcoes")!.style.width = "0";
 }
