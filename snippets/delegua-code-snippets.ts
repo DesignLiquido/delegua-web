416e641da821f5ce2263913930ae49751693a03b
Adolfo Neto <adolfo.usp@gmail.com>
2026-03-13 11:22:51 -0300

    Correção

--- snippets/delegua-code-snippets.ts
+++ snippets/delegua-code-snippets.ts
@@ -49,7 +49,7 @@ const deleguaCodeSnippets = [
 	{
 		prefixo: "se senao",
 		corpo: [
-			"se escreva(${1:condicao}) {",
+			"se (${1:condicao}) {",
 			"\tescreva('correspondente 1');",
 			"} senao {",
 			"\tescreva('correspondente 2');",
